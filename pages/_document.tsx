715067a89fd6e872c29a78bd3fd2675a8380aa01
Kyle Williams <kwilliams@peakactivity.com>
2020-04-24 09:05:04 -0400

    crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <html>
         <Head>
             <title>Grammerhub | Home</title>
-            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
         </Head>
         <body>
           <Main />

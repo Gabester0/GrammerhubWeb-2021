bbdb23faf981b1e490d7e70ed6449a5e8ee5b2bd
Steve Smodish <smodish@hotmail.com>
2020-05-03 23:41:02 -0400

    Render a few cards, Added todo list

--- pages/media.tsx
+++ pages/media.tsx
@@ -1,18 +1,59 @@
 import Layout from "../components/MyLayout";
-import { Container, Jumbotron, Row, Col, Media, Card } from 'react-bootstrap'
+import { Container, Jumbotron, Row, Col, Card } from 'react-bootstrap'
+
+// pull data from db
+// conditionally render loading images
+// how to view? clickable card, button, view in a player component
+// Lose Jumbotron?
+// adjust vertical spacing of cards
+// add media query to navbar bumper div: 24px small, 52px large
+
 
 export default () => (
   <Layout>
     <div style={{ height: '52px', marginTop: '15px' }}></div>
       <Jumbotron>
-          <h1>Video</h1>
+          <h1>Media</h1>
       </Jumbotron>
       <Container>
         <Row>
           <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.img 
-                src="http://placekitten.com/90/90"
+            <Card style={{ width: '20rem' }}>
+              <Card.Img 
+                src="http://placekitten.com/1800/1800"
+              />
+              <Card.Body>
+                <Card.Title>Bacon ipsum dolor amet</Card.Title>
+                <Card.Text>esse alcatra leberkas, elit hamburger qui drumstick et pastrami.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img 
+                src="http://placekitten.com/1500/1500"
+              />
+              <Card.Body>
+                <Card.Title>Bacon ipsum dolor amet</Card.Title>
+                <Card.Text>esse alcatra leberkas, elit hamburger qui drumstick et pastrami.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img 
+                src="http://placekitten.com/1900/1900"
+              />
+              <Card.Body>
+                <Card.Title>Bacon ipsum dolor amet</Card.Title>
+                <Card.Text>esse alcatra leberkas, elit hamburger qui drumstick et pastrami.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: '20rem' }}>
+              <Card.Img 
+                src="http://placekitten.com/1200/1200"
               />
               <Card.Body>
                 <Card.Title>Bacon ipsum dolor amet</Card.Title>

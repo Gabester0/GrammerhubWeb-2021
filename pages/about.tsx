1e5ec32301a3b6b2973e6f559caba39414c590de
Emily <emilyherr94@gmail.com>
2020-05-04 23:47:14 -0400

    pages/about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,53 +1,68 @@
+  
 import Layout from '../components/MyLayout'
-import css from './Header.scss'
 import { Row, Container, Col } from 'react-bootstrap'
+import { Jumbotron} from 'react-bootstrap'
 
 
 
 export default () => (
   <Layout>
 
-    <Row className="row justify-content-center mt-1 pt-4">
-      <div className="card col-lg-12card bg-dark text-white">
-        <img className="card-img" src="https://place-hold.it/820x260" alt="Card image"></img>
-        <div className="card-img-overlay">
-          <h1 className="text-center">Welcome to Grammerhub!</h1>
+<div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Fluid jumbotron</h1>
+          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </Container>
+      </Jumbotron>
+    </div>
+
+    <Container fluid>
+      <Row className= {'${css.styling}'}>
+        <Col sm={12} lg={6}>
+        
+          <div className={'${css.icon}'}>
+            <h5>About Us</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </Col>
+
+        <Col sm={12} lg={6}>
+        
+        <div className={'${css.icon}'}>
+          <h5>News</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-      </div>
-    </Row>
+      </Col>
 
-    <Container className="container-fluid pt-4">
-      <Row className="row justify-content-center">
-        <Col sm={12} className="card col-lg-4 pl-4 pr-4 ml-4 mr-4">
-          <img className="card-img-top" src="https://www.fillmurray.com/400/400" alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title text-center">About Us</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        </Col>
-
-
-        <Col sm={12} className="card col-lg-4 pl-4 pr-4 ml-4 mr-4">
-          <img className="card-img-top" src="https://www.fillmurray.com/400/400" alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title text-center">In the News</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        </Col>
       </Row>
-    </Container>
+    </Container>                                                                                                                                                                                                                          
 
-    <Row className="row justify-content-center mt-1 pt-4">
-      <div className="card col-lg-12card bg-dark text-white">
-        <img className="card-img" src="https://place-hold.it/820x260" alt="Card image"></img>
-        <div className="card-img-overlay">
-          <h1 className="text-center">Recent Projects</h1>
+    <Container fluid>
+      <Row className= {'${css.styling}'}>
+        <Col sm={12} lg={12}>
+        
+          <div className={'${css.icon}'}>
+            <h5>Recent Projects</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </Col>
+        </Row>
+        <Row className= {'${css.styling}'}>
 
-
+        <Col sm={12} lg={12}>
+        
+        <div className={'${css.icon}'}>
+          <h5>Last Class</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-      </div>
+      </Col>
+
+ 
+    
     </Row>
 
-
+</Container>
   </Layout>
 )
+

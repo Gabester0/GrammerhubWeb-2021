d27042605259f151dc07af7eb08970e4da2b51c0
Donald Murillo <donald.murillo07@gmail.com>
2020-05-07 20:38:39 -0400

    event getter

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,10 +1,13 @@
 import Layout from '../components/MyLayout'
 import MyCalendar from '../components/MyCalendar'
 import '../node_modules/react-big-calendar/lib/css/react-big-calendar.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const layoutStyle = {
     display: 'grid',
     gridGap: '10px',
+    marginTop: '50px',
     leftSide: {
         gridColumn: '1 / span 1'
     },
@@ -13,16 +16,42 @@ const layoutStyle = {
     }
 }
 
+const url: string = "https://api.meetup.com/grammerhub/events";
 
-export default () => (
-  <Layout> 
-    <div style={layoutStyle}>
-        <div style={layoutStyle.leftSide}>
-            this is the left side
-        </div>
-        <div style={layoutStyle.rightSide}>
-            <MyCalendar/>
-        </div>
-    </div>
-  </Layout>
-)
+
+
+export default () => {
+    // const [events, setEvents] = useState();
+
+    // const getEvents = () => axios.get(url);
+
+    const [hasError, setErrors] = useState(false);
+    const [events, setEvents] = useState([]);
+
+    async function fetchData() {
+        axios.get(url, {headers: {"Access-Control-Allow-Origin": "*"}})
+        .then(res => {
+          const events = res.data;
+          setEvents(events);
+        })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
+    return (
+        <Layout> 
+            <div style={layoutStyle}>
+                <div style={layoutStyle.leftSide}>
+                    this is the left side {events}
+                </div>
+                <div style={layoutStyle.rightSide}>
+                    <MyCalendar/>
+                </div>
+            </div>
+        </Layout>   
+    )
+}
+

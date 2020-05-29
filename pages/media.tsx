3972e67bdbf35853a90d9ab02b39e1c93c90a11c
Steve Smodish <smodish@hotmail.com>
2020-05-28 21:36:29 -0400

    Split out card and mapped off data

--- pages/media.tsx
+++ pages/media.tsx
@@ -1,7 +1,9 @@
 import Layout from "../components/MyLayout";
 import { Container, Jumbotron, Row, Col, Card } from 'react-bootstrap'
-
+import MediaCard from '../components/Media/MediaCard'
 // pull data from db
+let data = [{ id: 1, link: "https://www.youtube.com/embed/OqDANG7tCFc", title: "Grammerhub 002: React Part II", text: "Our March 16th 2020 meetup"}, { id: 2, link: "https://www.youtube.com/embed/wnA_mWbzvkc", title: "Placeholder 1", text: "Placeholder"}, { id: 3, link: "https://www.youtube.com/embed/tpiyEe_CqB4", title: "Placeholder 2", text: "Placeholder"}, { id: 4, link: "https://www.youtube.com/embed/Uj1ykZWtPYI", title: "Placeholder 3", text: "Placeholder"}]
+
 // conditionally render loading images
 // how to view? clickable card, button, view in a player component
 // Lose Jumbotron? <-- depends on site style
@@ -17,49 +19,9 @@ export default () => (
       </Jumbotron>
       <Container>
         <Row>
-          <Col>
-            <Card style={{ width: '20rem', marginBottom: "10px" }}>
-              <Card.Img 
-                src="http://placekitten.com/1800/1800"
-              />
-              <Card.Body>
-                <Card.Title>Bacon ipsum dolor amet</Card.Title>
-                <Card.Text>esse alcatra leberkas, elit hamburger qui drumstick et pastrami.</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '20rem', marginBottom: "10px" }}>
-              <iframe width="100%" height="318" src="https://www.youtube.com/embed/OqDANG7tCFc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-              </iframe>
-              <Card.Body>
-                <Card.Title>Grammerhub 002: React Part II</Card.Title>
-                <Card.Text>Our March 16th 2020 meetup</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '20rem', marginBottom: "10px" }}>
-              <Card.Img 
-                src="http://placekitten.com/1900/1900"
-              />
-              <Card.Body>
-                <Card.Title>Bacon ipsum dolor amet</Card.Title>
-                <Card.Text>esse alcatra leberkas, elit hamburger qui drumstick et pastrami.</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '20rem', marginBottom: "10px" }}>
-              <Card.Img 
-                src="http://placekitten.com/1200/1200"
-              />
-              <Card.Body>
-                <Card.Title>Bacon ipsum dolor amet</Card.Title>
-                <Card.Text>esse alcatra leberkas, elit hamburger qui drumstick et pastrami.</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+          {data.map(video => 
+            <MediaCard key={video.id} link={video.link} title={video.title} text={video.text} />
+          )}
         </Row>
       </Container>
   </Layout>

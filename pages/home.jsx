be6bd7f111292c4f6280d1cc94e9b1089d05182d
Kyle Williams <kwilliams@peakactivity.com>
2020-06-05 21:01:35 -0400

    capital home file

--- pages/home.jsx
+++ pages/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/MyLayout";
 import Services from "../components/Services/Services";
 import LastMeetups from "../components/Meetups/LastMeetups";
-import Hero from "../components/Home/Hero/Hero2";
+import Hero from "../components/Home/Hero/Hero";
 
 const Home = () => {
   return (

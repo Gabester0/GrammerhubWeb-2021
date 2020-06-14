f7b02cd66136def9cf3b52f477300ce5d145ea96
Donald Murillo <donald.murillo07@gmail.com>
2020-06-14 00:13:49 -0400

    - bug fixes

--- components/Home/Charts/Charts.jsx
+++ components/Home/Charts/Charts.jsx
@@ -5,11 +5,11 @@ import css from "./Charts.scss";
 
 const Charts = () => {
   return (
-    <div class={`${utils.section} ${css.chartMeetup}`}>
+    <div className={`${utils.section} ${css.chartMeetup}`}>
       <Container>
         <Row>
           <Col lg={12}>
-            <div class={css.describeChart}>
+            <div className={css.describeChart}>
               <h4>Build charts on a Dashboard</h4>
               <h2>Learn how to use open source libraries.</h2>
               <p>
@@ -26,10 +26,10 @@ const Charts = () => {
                 {"Seek More"}
               </Button>
             </div>
-            <div class={css.sampleChart}>
+            <div className={css.sampleChart}>
               <img
                 src="https://place-hold.it/400x300"
-                class="img-fluid"
+                className="img-fluid"
                 alt="Doghnut Chart"
               />
             </div>
--- components/MyCalendar.tsx
+++ components/MyCalendar.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Calendar, Views, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import { MeetupEvent } from "service/eventModel";
+import Layout from "./MyLayout";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import EventListItem from "./EventListItem";
+import "../node_modules/react-big-calendar/lib/css/react-big-calendar.css";
+import css from '../components/MyCalendar.scss'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 const localizer = momentLocalizer(moment);
 const allViews = Object.keys(Views).map((k) => Views[k]);
@@ -13,10 +22,49 @@ const ColoredDateCellWrapper = ({ children }) =>
   });
 
 const MyCalendar = (props) => {
-  const events = props.events.map((event) => {
-    let eventTime: number[] = event.local_time.split(":");
-    let eventDate: number[] = event.local_date.split("-");
-    let newDateStart = new Date(eventDate[0], eventDate[1] - 1, eventDate[2], eventTime[0], eventTime[1]);
+
+  // ------------ HOOKS ------------- //
+  const [events, setEvents] = React.useState<MeetupEvent[]>([]);
+  const [searchTerm, setSearchTerm] = React.useState<string>('');
+  const [filtered, setFiltered] = React.useState<MeetupEvent[]>([]);
+
+  // ON LOAD GET EVENTS FROM MEETUP
+  React.useEffect(() => {
+    fetchData();
+    setFiltered(events);
+  }, []);
+
+  // React.useEffect(() => {
+  //   setFiltered(events);
+  // }, [])
+
+  // WHEN searchTerm CHANGES AND ITS GREATERT THAN 3 FILTER EVENTS
+  React.useEffect(() => {
+    filter();
+  }, [searchTerm])
+
+  // ----------- FUNCTIONS ------------- //
+  // ASYNC GET EVENTS FROM MEETUP USING THE BACK END
+  async function fetchData() {
+    axios.get("/grammerhub").then((res) => {
+      setEvents(res.data);
+      setFiltered(res.data);
+    });
+  }
+
+  const filter = () => {
+    if (searchTerm.length < 3) return;
+    const results = events.filter(event => 
+      JSON.stringify(Object.values(event)).toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    setFiltered(results);
+  }
+
+  const myEvents = (events: MeetupEvent[]) => events.map((event) => {
+    let eventTime: any = event.local_time?.split(":");
+    let eventDate: any = event.local_date?.split("-");
+    let newDateStart = new Date(
+      parseInt(eventDate[0]), parseInt(eventDate[1]) - 1, parseInt(eventDate[2]), parseInt(eventTime[0]), parseInt(eventTime[1]));
     let newDateEnd = new Date( new Date(newDateStart).setHours(newDateStart.getHours() + 2));
     return {
       allDay: false,
@@ -25,23 +73,61 @@ const MyCalendar = (props) => {
       title: event.name,
     };
   });
+
+  // --------- HANDLERS ----------- ///
+  const handleChange = event => {
+    if (event?.target?.value === '' && event?.target?.value?.length < searchTerm.length) setFiltered(events);
+    setSearchTerm(event.target.value);
+  }
+
+  const handleClick = event => {
+    filter();
+  }
   return (
-    <div style={{ minHeight: 500 }}>
-      <Calendar
-        events={events}
-        views={allViews}
-        step={60}
-        showMultiDayTimes
-        defaultDate={new Date()}
-        components={{
-          timeSlotWrapper: ColoredDateCellWrapper,
-        }}
-        startAccessor="start"
-        endAccessor="end"
-        localizer={localizer}
-        style={{ height: 500 }}
-      />
-    </div>
+    <Layout>
+      <Row className={css.layoutStyle}>
+        <Col className={css.leftSide}>
+          <div className={css.eventHeader}>Events List</div>
+          <label className={css.labelSearch}>
+            Search: 
+            <input 
+              type="text" 
+              name="searchBar" 
+              placeholder="filter"
+              className={css.searchInput}
+              value={searchTerm} 
+              onInput={handleChange} 
+            />
+              <button type="submit" onClick={handleClick}><FontAwesomeIcon icon={faSearch}/></button>
+          </label>
+          <div className={css.eventList}>
+            {filtered.map((event, index) => {
+              return (
+                <EventListItem event={event} index={index}></EventListItem>
+              );
+            })}
+          </div>
+        </Col>
+        <Col className={css.rightSide}>
+          <div style={{ minHeight: 500 }}>
+            <Calendar
+              events={myEvents(filtered)}
+              views={allViews}
+              step={60}
+              showMultiDayTimes
+              defaultDate={new Date()}
+              components={{
+                timeSlotWrapper: ColoredDateCellWrapper,
+              }}
+              startAccessor="start"
+              endAccessor="end"
+              localizer={localizer}
+              style={{ height: 500 }}
+            />
+          </div>
+        </Col>
+      </Row>
+    </Layout>
   );
 };
 

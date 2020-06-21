2b41bdc0e1cb3e8c7ebbac6d20462c05a7cbb0f2
Gabester0 <gceipper@gmail.com>
2020-06-21 11:43:12 -0700

    Trying to pull develop

---
+++ components/Navbar/CollapseNavbar.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header";
+import HeaderCTA from "./HeaderCTA";
+
+const CollapseNavbar = () => (
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <Header />
+    {/* <HeaderCTA /> */}
+  </div>
+);
+
+export default CollapseNavbar;

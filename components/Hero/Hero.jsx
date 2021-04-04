c3f9b32c7b34a509ee99bd8fa244f9d1575a69af
Gabester0 <gceipper@gmail.com>
2021-04-04 13:57:24 -0400

    added next Event link using previous call to Meetup to get upcoming scheduled meetups

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import axios from 'axios';
 
@@ -8,16 +8,29 @@ import utils from "../Helpers.scss";
 import css from "./Hero.scss";
 
 export const Hero = ()=> {
+  const [events, setEvents] = useState([]);
+  const [nextEventDate, setNextEventDate] = useState(null);
 
-  const [events, setEvents] = React.useState([]);
   React.useEffect(() => {
     fetchData();
   }, []);
 
   async function fetchData() {
-    axios.get("/grammerhub").then((res) => {
-      setEvents(res.data);
-      console.log(res.data)
+    axios.get("/grammerhub").then(({ data }) => {
+      setEvents(data);
+      console.log(data[0])
+      const dateArr = new Date(`${data[0].local_date}T${data[0].local_time}`).toString().split("");
+      dateArr.splice(dateArr.length - 4);
+      //If 3rd from last char is "t" string is Eastern Daylight Time and need to splice 8 chars ("aylight ") else splice 7 chars ("avings ")
+      if(dateArr[dateArr.length -3] === "t") dateArr.splice(dateArr.length - 9, 8);
+      else dateArr.splice(dateArr.length - 9, 7);
+      dateArr.splice(dateArr.length - 9, 7);
+      dateArr.splice(dateArr.length - 13, 10);
+      dateArr.splice(dateArr.length - 7, 3);
+      const newHour = (parseInt(dateArr.slice(dateArr.length - 9, dateArr.length - 7).join("")) - 12).toString().padStart(2, "0");
+      dateArr.splice(dateArr.length - 9, 2, newHour);
+      dateArr.splice(dateArr.length - 3, 0, "PM ");
+      setNextEventDate(dateArr.join(""));
     });
   }
 
@@ -43,12 +56,12 @@ export const Hero = ()=> {
                     -Bump of the font sizes for everything on the landing page except for the button
                     -Decide how to visually link Grammer in h1 and in strong (light purple? italics)
                     -Make sure the individual text blocks in h6 break into columns (3 separate h6 display inline?  Need to address pipes)
-                    -Add events[0] (next event) to Hero
+                    -Change link color
                   */}
                   <div className={`${css.heroMain} ${css.pb30}`}>
                     <h6>Live Coding | Dev Community | Career Advice</h6>
+                    <p>Next online event: <a href={events[0]?.link || 'https://www.meetup.com/grammerhub/events/'}>{nextEventDate || ""}</a></p>
                   </div>
-
                   <div className={css.heroContainer}>
                     <Button 
                       bsPrefix={css.heroBtn} 
@@ -58,7 +71,6 @@ export const Hero = ()=> {
                       {"Join Our Slack"}
                     </Button>
                   </div>
-
                 </Col>
               </Row>
             </Container>
